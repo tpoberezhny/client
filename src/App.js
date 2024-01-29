83468f9141387ec457a5ccdb298eae9af8fc47f3
Tymofii Poberezhnyi <tymofii.poberezhnyi@publicisgroupe.cz>
2024-01-29 14:22:39 +0100

    Skeleton of App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>Expense Tracker</h1>
+      <div className="container mx-auto max-w-6xl text-center drop-shadow-lg text-gray-800">
+        <h1 className="text-4xl py-8 mb-10 bg-slate-800 text-white rounded">
+          Expense Tracker
+        </h1>
+        {/*grid columns */}
+        <div className="grid md:grid-cols-2 gap-4">
+          {/*Chart */}
+          {/*Form */}
+        </div>
+      </div>
     </div>
   );
 }
