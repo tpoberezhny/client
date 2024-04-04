d0d0de87dc314db4f0f47fb8ad24ac66cfc0577a
Tymofii Poberezhnyi <tymofii.poberezhnyi@publicisgroupe.cz>
2024-04-04 11:14:24 +0200

    Adding function for automatically changing month name

--- src/App.js
+++ src/App.js
@@ -2,12 +2,22 @@ import "./App.css";
 import Graph from "./components/Graph";
 import Form from "./components/Form";
 
+function getCurrentMonthInRussian() {
+  const monthsInRussian = [
+    "Январь", "Февраль", "Март", "Апрель", "Май", "Июнь",
+    "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь"
+  ];
+  
+  const currentMonthIndex = new Date().getMonth();
+  return monthsInRussian[currentMonthIndex];
+}
+
 function App() {
   return (
     <div className="App">
       <div className="container mx-auto max-w-6xl text-center drop-shadow-lg text-gray-800">
         <h1 className="text-4xl py-8 mb-10 bg-[#fb7185] text-white rounded">
-          История Расходов (Март)
+          История Расходов ({getCurrentMonthInRussian()})
         </h1>
         {/*grid columns */}
         <div className="grid md:grid-cols-2 gap-4">
